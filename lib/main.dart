8dad5394761299eebc56b26cfaa06b2899b93ba7
msdhanga <mohammaddhanga2@gmail.com>
2023-04-27 09:26:55 +0530

    fibonacci

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main(){
@@ -12,35 +14,109 @@ class MyApp extends StatelessWidget {
      return MaterialApp(
          home: Scaffold(
             body: SafeArea(
-              child: Row(
-                 children:[
-                   Container(
-                     padding: EdgeInsets.all(20),
-                     color: Colors.red,
-                     child: Text('1'),
-                   ),
+                 child: Column(
+                   children: [
+                     _customerContainer(
+                         text1: 'Mohammad',
+                          text: 'Dhanga'),
+                     _customerContainer1(
+                       number: 8,
+                       color: Colors.red
+                     ),
                    Expanded(
-                     flex: 7,
-                     child: Container(
-                     padding: EdgeInsets.all(20),
-                     color: Colors.greenAccent,
-                     child: Text('2'),
-                       ),
-                      ),
-                   Expanded(
-                     flex: 3,
-                     child: Container(
-                     padding: EdgeInsets.all(20),
-                     color: Colors.blueGrey,
-                     child: Text('3'),
-                    ),
-                   ),
-                 ], // children
-              ),
+                     flex: 5,
+                       child:Row(
+                         children: [
+                           _customerContainer1(
+                               number: 5,
+                               color: Colors.indigo
+                           ),
+                           Expanded(
+                             flex: 3,
+                               child: Column(
+                                 children: [
+                                   Expanded(
+                                     flex: 2,
+                                       child: Row(
+                                         children: [
+                                           Expanded(
+                                             flex: 1,
+                                               child: Column(
+                                                 children: [
+                                                   _customerContainer1(
+                                                       number: 1,
+                                                       color: Colors.green
+                                                   ),
+                                                   _customerContainer1(
+                                                       number: 1,
+                                                       color: Colors.blue,
+                                                   ),
+                                                 ],
+                                               )),
+                                           _customerContainer1(
+                                               number: 2,
+                                               color: Colors.brown
+                                           ),
+                                         ],
+                                       )),
+                                   _customerContainer1(
+                                       number: 3,
+                                       color: Colors.purple
+                                   ),
+
+                                 ],
+                               ))
+                         ],
+                       ))
+                   /* Expanded(
+                       flex: 8,
+                       child: Container(
+                       constraints: BoxConstraints.expand(),
+                       color: Colors.red,
+                       child: Center(child: Text('8',style: TextStyle(
+                         color: Colors.white,
+                         fontSize: 40,
+                       ),)),
+                     ),
+                     ),*/
+                    /* Expanded(
+                       flex: 5,
+                       child: Container(
+                       color: Colors.indigo,
+                       child: Center(child: Text('5',style: TextStyle(
+                         color: Colors.white,
+                         fontSize: 40,
+                       ),)),
+                     )
+                    )*/
+                   ],
+                 )
             ),
          ),
      );
   }
 }
 
+Widget _customerContainer({
+    required String text,
+    required String text1}){
+  //return Text('hello world');
+  return Text('${text} - ${text1}');
+}
 
+Widget _customerContainer1({
+  required int number,
+  required Color color,
+}){
+   return Expanded(
+     flex: number,
+     child: Container(
+     constraints: BoxConstraints.expand(),
+     color: color,
+     child: Center(child: Text(number.toString() ,style: TextStyle(
+     color: Colors.white,
+     fontSize: 25,
+     ),)),
+    ),
+   );
+ }
